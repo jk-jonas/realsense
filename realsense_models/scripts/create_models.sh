c64671486b35fbb496e44ec05f9e664bd2b4e382
Jonas Kälble <j.kaeble@gmail.com>
2020-04-17 15:34:09 +0000

    create paths on build

--- realsense_models/scripts/create_models.sh
+++ realsense_models/scripts/create_models.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # create the models for gazebo with plugins
 for name in "d435" "d415"; do
+    mkdir -p "$(rospack find realsense_models)/urdf"
+    mkdir -p "$(rospack find realsense_models)/sdf"
     rosrun xacro xacro "$(rospack find realsense2_description)/urdf/test_${name}_camera.urdf.xacro" > "$(rospack find realsense_models)/urdf/rs_${name}_camera.urdf"
     gz sdf -p "$(rospack find realsense_models)/urdf/rs_${name}_camera.urdf" > "$(rospack find realsense_models)/sdf/rs_${name}_camera.sdf"
     sed -i "s/model:\/\/realsense2_description\//file:\/\//g" "$(rospack find realsense_models)/sdf/rs_${name}_camera.sdf"
